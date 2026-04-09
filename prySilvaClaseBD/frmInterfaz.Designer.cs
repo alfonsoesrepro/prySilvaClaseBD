57dcf8e5cefc4bc1519c7d3464d9ee023ba6c831
alfonsoesrepro <alfonsoesrepro@gmail.com>
2026-04-09 10:07:54 -0300

    Agregar archivos de proyecto.

---
+++ prySilvaClaseBD/frmInterfaz.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace prySilvaClaseBD
+{
+    partial class frmInterfaz
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cboDatabases = new System.Windows.Forms.ComboBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnCargar = new System.Windows.Forms.Button();
+            this.cboTables = new System.Windows.Forms.ComboBox();
+            this.dgvData = new System.Windows.Forms.DataGridView();
+            this.lblDatabases = new System.Windows.Forms.Label();
+            this.lblTables = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvData)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cboDatabases
+            // 
+            this.cboDatabases.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboDatabases.FormattingEnabled = true;
+            this.cboDatabases.Location = new System.Drawing.Point(12, 29);
+            this.cboDatabases.Name = "cboDatabases";
+            this.cboDatabases.Size = new System.Drawing.Size(460, 21);
+            this.cboDatabases.TabIndex = 0;
+            this.cboDatabases.SelectedIndexChanged += new System.EventHandler(this.cboDatabases_SelectedIndexChanged);
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(478, 27);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(75, 23);
+            this.btnAgregar.TabIndex = 1;
+            this.btnAgregar.Text = "Agregar...";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // btnCargar
+            // 
+            this.btnCargar.Location = new System.Drawing.Point(559, 27);
+            this.btnCargar.Name = "btnCargar";
+            this.btnCargar.Size = new System.Drawing.Size(75, 23);
+            this.btnCargar.TabIndex = 2;
+            this.btnCargar.Text = "Cargar";
+            this.btnCargar.UseVisualStyleBackColor = true;
+            this.btnCargar.Click += new System.EventHandler(this.btnCargar_Click);
+            // 
+            // cboTables
+            // 
+            this.cboTables.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboTables.FormattingEnabled = true;
+            this.cboTables.Location = new System.Drawing.Point(12, 74);
+            this.cboTables.Name = "cboTables";
+            this.cboTables.Size = new System.Drawing.Size(460, 21);
+            this.cboTables.TabIndex = 3;
+            this.cboTables.SelectedIndexChanged += new System.EventHandler(this.cboTables_SelectedIndexChanged);
+            // 
+            // dgvData
+            // 
+            this.dgvData.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvData.Location = new System.Drawing.Point(12, 111);
+            this.dgvData.Name = "dgvData";
+            this.dgvData.Size = new System.Drawing.Size(776, 327);
+            this.dgvData.TabIndex = 4;
+            // 
+            // lblDatabases
+            // 
+            this.lblDatabases.AutoSize = true;
+            this.lblDatabases.Location = new System.Drawing.Point(12, 9);
+            this.lblDatabases.Name = "lblDatabases";
+            this.lblDatabases.Size = new System.Drawing.Size(136, 13);
+            this.lblDatabases.TabIndex = 5;
+            this.lblDatabases.Text = "Archivos de base de datos:";
+            // 
+            // lblTables
+            // 
+            this.lblTables.AutoSize = true;
+            this.lblTables.Location = new System.Drawing.Point(12, 58);
+            this.lblTables.Name = "lblTables";
+            this.lblTables.Size = new System.Drawing.Size(42, 13);
+            this.lblTables.TabIndex = 6;
+            this.lblTables.Text = "Tablas:";
+            // 
+            // frmInterfaz
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblTables);
+            this.Controls.Add(this.lblDatabases);
+            this.Controls.Add(this.dgvData);
+            this.Controls.Add(this.cboTables);
+            this.Controls.Add(this.btnCargar);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.cboDatabases);
+            this.Name = "frmInterfaz";
+            this.Text = "Visor de Bases de Datos";
+            this.Load += new System.EventHandler(this.frmInterfaz_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvData)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cboDatabases;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnCargar;
+        private System.Windows.Forms.ComboBox cboTables;
+        private System.Windows.Forms.DataGridView dgvData;
+        private System.Windows.Forms.Label lblDatabases;
+        private System.Windows.Forms.Label lblTables;
+    }
+}
+
---
+++ prySilvaClaseBD/frmInterfaz.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+using System.IO;
+
+namespace prySilvaClaseBD
+{
+    public partial class frmInterfaz : Form
+    {
+        private CConexion conexionActual;
+
+        public frmInterfaz()
+        {
+            InitializeComponent();
+        }
+
+        private void frmInterfaz_Load(object sender, EventArgs e)
+        {
+            // Inicializar controles
+            cboDatabases.Items.Clear();
+            cboTables.Items.Clear();
+            dgvData.DataSource = null;
+
+            // Poblar combobox buscando archivos en la carpeta ClaseBD del proyecto
+            PopulateDatabasesFromClaseBDFolder();
+        }
+
+        private void PopulateDatabasesFromClaseBDFolder()
+        {
+            string baseDir = AppDomain.CurrentDomain.BaseDirectory;
+            var candidateDirs = new List<string>
+            {
+                Path.Combine(baseDir, "ClaseBD"),
+                Path.GetFullPath(Path.Combine(baseDir, "..", "..", "ClaseBD")),
+                Path.GetFullPath(Path.Combine(baseDir, "..", "ClaseBD"))
+            };
+
+            var added = new HashSet<string>(StringComparer.OrdinalIgnoreCase);
+
+            foreach (var dir in candidateDirs)
+            {
+                try
+                {
+                    if (Directory.Exists(dir))
+                    {
+                        var mdbs = Directory.GetFiles(dir, "*.mdb");
+                        var accdbs = Directory.GetFiles(dir, "*.accdb");
+                        foreach (var f in mdbs.Concat(accdbs))
+                        {
+                            if (!added.Contains(f))
+                            {
+                                added.Add(f);
+                                cboDatabases.Items.Add(f);
+                            }
+                        }
+                    }
+                }
+                catch
+                {
+                    // ignorar errores
+                }
+            }
+
+            if (cboDatabases.Items.Count > 0)
+                cboDatabases.SelectedIndex = 0;
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+                ofd.Filter = "Access Databases (*.mdb;*.accdb)|*.mdb;*.accdb|All files (*.*)|*.*";
+                ofd.Title = "Seleccionar archivo de base de datos";
+
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    string ruta = ofd.FileName;
+                    if (!cboDatabases.Items.Contains(ruta))
+                        cboDatabases.Items.Add(ruta);
+                    cboDatabases.SelectedItem = ruta;
+                }
+            }
+        }
+
+        private void btnCargar_Click(object sender, EventArgs e)
+        {
+            if (cboDatabases.SelectedItem == null)
+            {
+                MessageBox.Show("Seleccione un archivo de base de datos primero.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            string ruta = cboDatabases.SelectedItem.ToString();
+            string ext = Path.GetExtension(ruta)?.ToLowerInvariant();
+
+            // Lista de proveedores a intentar en orden de preferencia
+            var providersToTry = new List<string>();
+            if (ext == ".accdb")
+            {
+                providersToTry.Add("Provider=Microsoft.ACE.OLEDB.16.0;");
+                providersToTry.Add("Provider=Microsoft.ACE.OLEDB.12.0;");
+            }
+            // Siempre agregar Jet como último recurso (solo .mdb es soportado oficialmente)
+            providersToTry.Add("Provider=Microsoft.Jet.OLEDB.4.0;");
+
+            if (conexionActual != null)
+            {
+                conexionActual.Desconectar();
+                conexionActual = null;
+            }
+
+            conexionActual = new CConexion();
+            bool conectado = false;
+            string usedProvider = null;
+            string lastError = null;
+
+            foreach (var prov in providersToTry)
+            {
+                try
+                {
+                    string tryConn = prov + "Data Source=" + ruta + ";";
+                    // asegurar desconexión antes de intentar
+                    conexionActual.Desconectar();
+                    if (conexionActual.Conectar(tryConn))
+                    {
+                        conectado = true;
+                        usedProvider = prov;
+                        break;
+                    }
+                    else
+                    {
+                        lastError = conexionActual.ObtenerError();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    lastError = ex.Message;
+                }
+            }
+
+            if (conectado)
+            {
+                if (!string.IsNullOrEmpty(usedProvider) && usedProvider.IndexOf("ACE", StringComparison.OrdinalIgnoreCase) >= 0 && usedProvider.IndexOf("16.0") >= 0)
+                {
+                    // opcional: informar si se conectó con ACE 16.0
+                    // MessageBox.Show("Conectado usando ACE 16.0.", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else if (!string.IsNullOrEmpty(usedProvider) && usedProvider.IndexOf("Jet", StringComparison.OrdinalIgnoreCase) >= 0)
+                {
+                    // Si se conectó con Jet y archivo es .accdb, advertir que el resultado puede ser limitado
+                    if (ext == ".accdb")
+                    {
+                        MessageBox.Show("Se ha conectado usando el proveedor Jet como alternativa. Tenga en cuenta que Jet no soporta correctamente archivos .accdb; instale ACE si experimenta problemas.", "Conexión alternativa", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    }
+                    else
+                    {
+                        // conectado con Jet y es .mdb: todo bien
+                    }
+                }
+
+                try
+                {
+                    cboTables.Items.Clear();
+                    dgvData.DataSource = null;
+
+                    DataTable tablas = conexionActual.CNN.GetSchema("Tables");
+                    foreach (DataRow row in tablas.Rows)
+                    {
+                        string tableType = row["TABLE_TYPE"].ToString();
+                        string tableName = row["TABLE_NAME"].ToString();
+                        if (string.Equals(tableType, "TABLE", StringComparison.OrdinalIgnoreCase))
+                        {
+                            if (!tableName.StartsWith("MSys", StringComparison.OrdinalIgnoreCase))
+                                cboTables.Items.Add(tableName);
+                        }
+                    }
+
+                    if (cboTables.Items.Count > 0)
+                        cboTables.SelectedIndex = 0;
+                    else
+                        MessageBox.Show("No se encontraron tablas en la base de datos.", "Información", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error al obtener tablas: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                string err = lastError ?? conexionActual?.ObtenerError() ?? "";
+                // Mensaje instructivo si parece ser problema de proveedor
+                if (err.IndexOf("Microsoft.ACE.OLEDB", StringComparison.OrdinalIgnoreCase) >= 0
+                    || err.IndexOf("ACE.OLEDB", StringComparison.OrdinalIgnoreCase) >= 0
+                    || err.IndexOf("no está registrado", StringComparison.OrdinalIgnoreCase) >= 0
+                    || err.IndexOf("not registered", StringComparison.OrdinalIgnoreCase) >= 0)
+                {
+                    MessageBox.Show(
+                        "No se puede conectar: el proveedor OLEDB no está registrado o hay un conflicto de 32/64 bits.\n\n" +
+                        "Posibles soluciones:\n" +
+                        "1) Instalar 'Microsoft Access Database Engine' (2016 o 2010). Asegúrate de instalar la versión (32/64-bit) que coincida con la plataforma de la aplicación.\n" +
+                        "2) Cambiar la plataforma objetivo del proyecto en Visual Studio a x86 (Proyecto -> Propiedades -> Compilar -> Plataforma objetivo = x86) si instalaste la versión de 32 bits.\n" +
+                        "3) Si el archivo es .accdb, instala ACE; Jet no soporta .accdb.",
+                        "Proveedor no instalado", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    MessageBox.Show("No se pudo conectar: " + err, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+
+                conexionActual = null;
+            }
+        }
+
+        private void cboTables_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (conexionActual == null) return;
+            if (cboTables.SelectedItem == null) return;
+
+            string tabla = cboTables.SelectedItem.ToString();
+            try
+            {
+                string sql = $"SELECT * FROM [{tabla}]";
+                using (OleDbDataAdapter da = new OleDbDataAdapter(sql, conexionActual.CNN))
+                {
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+                    dgvData.DataSource = dt;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al cargar datos: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void cboDatabases_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
